d05361f3f6191b0694c555f3525e991f17a4b669
chuvak393 <chuvak393@gmail.com>
2020-03-22 23:42:20 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let res = str;
+    let config = bracketsConfig.map(item => item.join(''));
+    let bracket = '';
+    for (let j = 0; j < str.length; j++) {
+        for (let i=0; i < config.length; i++) {
+            if (res.includes(config[i])) {
+                bracket = config[i];
+                res = res.replace(bracket, '');
+                i--; 
+            }
+        }
+    }
+    return (res === '' ? true : false)
+  }
